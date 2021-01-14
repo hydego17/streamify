f62af2e283201ce5899d4f58533f543285817903
Umma Ahimsha <uahimsha@gmail.com>
2021-01-14 16:29:21 +0700

    + signin, signup, signout page

---
+++ app/pages/auth/signout.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react';
+import { useAuth } from 'lib/useAuth';
+
+export default function SignOut() {
+  const { signOut } = useAuth();
+
+  useEffect(() => {
+    signOut();
+  }, []);
+
+  return <div>Signout</div>;
+}
