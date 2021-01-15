3bc93255b665e88eb6fe02681f8907358e7a4c11
Umma Ahimsha <uahimsha@gmail.com>
2021-01-15 11:18:59 +0700

    + Layout w/ chakra-ui

---
+++ app/components/Layout/Header.tsx
@@ -0,0 +1,43 @@
+import Link from 'next/link';
+import { Container, Box, Flex, Button, Heading } from '@chakra-ui/react';
+
+import { useAuth } from 'lib/useAuth';
+import { ThemeSwitcher } from 'components/ThemeSwitcher';
+
+export default function Header() {
+  const { user } = useAuth();
+
+  const links = [
+    !user && { label: 'Sign Up', href: '/auth/signup' },
+    !user && { label: 'Sign In', href: '/auth/signin' },
+    user && { label: 'Streams', href: '/streams' },
+    user && { label: 'Create', href: '/streams/new' },
+    user && { label: 'Sign Out', href: '/auth/signout' },
+  ]
+    .filter((link) => link)
+    .map(({ label, href }) => {
+      return (
+        <Link href={href} key={href}>
+          <Button variant="ghost">{label} </Button>
+        </Link>
+      );
+    });
+
+  return (
+    <Box as="header" py={6}>
+      <Container maxW="3xl">
+        <Flex justify="space-between" align="center">
+          <Flex as="nav" align="center">
+            <Heading size="md" pr={4}>
+              <Link href="/">Streamify</Link>
+            </Heading>
+
+            {links}
+          </Flex>
+
+          <ThemeSwitcher />
+        </Flex>
+      </Container>
+    </Box>
+  );
+}
---
+++ app/components/Layout/index.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from 'react';
+import Header from 'components/Layout/Header';
+
+import { Container } from '@chakra-ui/react';
+
+type Props = {
+  children: ReactNode;
+};
+
+const Layout = ({ children }: Props) => {
+  return (
+    <>
+      <Header />
+
+      <Container maxW="3xl">{children}</Container>
+    </>
+  );
+};
+
+export default Layout;
