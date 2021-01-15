af4d05fcfab53e59b5a091953dd77d55f36006d8
Umma Ahimsha <uahimsha@gmail.com>
2021-01-15 10:45:57 +0700

    + Material-UI Provider, switched to Chakra-UI

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { ChakraProvider } from '@chakra-ui/react';
 
@@ -10,19 +9,6 @@ import Header from 'components/Header';
 
 export default function MyApp({ Component, pageProps }) {
   const apolloClient = useApollo(pageProps.initialApolloState);
-
-  const [darkState, setDarkState] = useState(false);
-  const handleThemeChange = () => {
-    setDarkState(!darkState);
-  };
-
-  useEffect(() => {
-    // Remove the server-side injected CSS
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
-  }, []);
 
   return (
     <ApolloProvider client={apolloClient}>
