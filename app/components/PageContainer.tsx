3e3d9f2bcd29bbf2c5d1ac30b1a0a161ae20b5df
Umma Ahimsha <uahimsha@gmail.com>
2021-01-15 12:26:54 +0700

    + maxW props

--- app/components/PageContainer.tsx
+++ app/components/PageContainer.tsx
@@ -1,9 +1,9 @@
 import { Box, Heading } from '@chakra-ui/react';
 
-export default function PageContainer({ children, title }) {
+export default function PageContainer({ children, title, maxW = 'md' }) {
   return (
     <>
-      <Box my={4} maxW="md">
+      <Box my={4} maxW={maxW}>
         <Box pb={4}>
           <Heading>{title}</Heading>
         </Box>
