d3e1607b319d5356d0f3d2bec8205c5d8c47f043
Umma Ahimsha <uahimsha@gmail.com>
2021-01-15 12:27:35 +0700

    + PageContainer

---
+++ app/pages/streams/index.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+
+import { useStreamsQuery, Stream } from 'lib/graphql/streams.graphql';
+
+import PageContainer from 'components/PageContainer';
+import Posts from 'components/Posts';
+
+export default function Streams() {
+  const { data, loading, refetch } = useStreamsQuery({ errorPolicy: 'ignore' });
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  return (
+    <PageContainer title="Streams" maxW="3xl">
+      {!loading && data && data.streams && (
+        <Posts streams={data.streams as Stream[]} />
+      )}
+    </PageContainer>
+  );
+}
