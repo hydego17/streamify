c11ce3f71815b3a9c730a996cfa62bc9e3be7811
Umma Ahimsha <uahimsha@gmail.com>
2021-01-21 16:53:16 +0700

    added background prop option

--- app/components/PageContainer.tsx
+++ app/components/PageContainer.tsx
@@ -1,9 +1,15 @@
 import { Box, Heading } from '@chakra-ui/react';
 
-export default function PageContainer({ children, title, maxW = 'md' }) {
+export default function PageContainer({
+  children,
+  title,
+  maxW = 'md',
+  background = null,
+}) {
   return (
     <>
-      <Box my={4} maxW={maxW}>
+      <Box my={4} maxW={maxW} bgImage={background} position="relative" p={6}>
+        {background && <PageOverlay />}
         <Box pb={4}>
           <Heading>{title}</Heading>
         </Box>
@@ -12,3 +18,15 @@ export default function PageContainer({ children, title, maxW = 'md' }) {
     </>
   );
 }
+
+const PageOverlay = () => (
+  <Box
+    position="absolute"
+    top={0}
+    bottom={0}
+    right={0}
+    left={0}
+    bgColor="rgba(0,0,0,0.3)"
+    p={6}
+  />
+);
