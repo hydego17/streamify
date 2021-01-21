330b4619adec57f39dd97d061e92dc4fa3983b0f
Umma Ahimsha <uahimsha@gmail.com>
2021-01-21 16:52:01 +0700

    change hero & content import on stream detail page

--- app/pages/streams/[id]/index.tsx
+++ app/pages/streams/[id]/index.tsx
@@ -2,8 +2,8 @@ import { GetServerSideProps } from 'next';
 import { Container } from '@material-ui/core';
 import { useStreamQuery, Stream } from 'lib/graphql/stream.graphql';
 
-import Hero from 'components/Hero';
-import Content from 'components/Content';
+import Hero from 'components/StreamDetail/Hero';
+import Content from 'components/StreamDetail/Content';
 
 export default function StreamDetail({ id }) {
   const { data, loading } = useStreamQuery({
